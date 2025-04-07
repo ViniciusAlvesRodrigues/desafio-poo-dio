762b141874274cefe4b0285f49f8f1c8ccb11377
VINICIUS RODRIGUES <viniciusalro@gmail.com>
2025-04-06 21:10:45 -0300

    Commit inicial

--- src/Main.java
+++ src/Main.java
@@ -22,13 +22,9 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        /*System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);*/
-
         Bootcamp bootcamp = new Bootcamp();
         bootcamp.setNome("Bootcamp Java Developer");
-        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");        
         bootcamp.getConteudos().add(curso1);
         bootcamp.getConteudos().add(curso2);
         bootcamp.getConteudos().add(mentoria);
